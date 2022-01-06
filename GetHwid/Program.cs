4260642051e0c8a1c914f42cc0adfe9c32f4bd36
Furkan Kadir Güzeloğlu <74448882+furkankadirguzeloglu@users.noreply.github.com>
2022-01-06 10:46:41 +0300

    Added Project

---
+++ GetHwid/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHwid
+{
+    internal class Program
+    {
+        public static string GetProcessorID()
+        {
+            string IDNumber = "";
+            ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("Select ProcessorID From Win32_processor");
+            foreach (ManagementObject managementObject in managementObjectSearcher.Get())
+            {
+                IDNumber += managementObject["ProcessorID"].ToString().Replace(" ", "");
+            }
+            return IDNumber;
+        }
+
+        public static string GetMotherBoardSerial()
+        {
+            string SerialNumber = "";
+            ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("SELECT SerialNumber FROM Win32_BaseBoard");
+            foreach (ManagementObject managementObject in managementObjectSearcher.Get())
+            {
+                SerialNumber += managementObject["SerialNumber"].ToString().Replace(" ", "");
+            }
+            return SerialNumber;
+        }
+
+        static string GetHDDSerial()
+        {
+            string SerialNumber = "";
+            ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject managementObject in managementObjectSearcher.Get())
+            {
+                SerialNumber += managementObject["SerialNumber"].ToString().Replace(" ", "") + "|";
+            }
+            return SerialNumber;
+        }
+
+        static string GetCustomHwid()
+        {
+            string CustomHwid = GetProcessorID() + "(!*!)" + GetMotherBoardSerial() + "(!*!)" + GetHDDSerial();
+            var stringBuilder = new StringBuilder();
+            var bytes = Encoding.Unicode.GetBytes(CustomHwid);
+            foreach (var i in bytes) {
+                stringBuilder.Append(i.ToString("X2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Custom Hwid: " + GetCustomHwid());
+        }
+    }
+}
